5017fef969662d3403b58b0a2270bdd03535eaeb
Alan <nhnam.develop@gmail.com>
2015-06-26 15:49:22 +0800

    Init

---
+++ NNActionSheet/NNActionSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NNActionSheet
+//
+//  Created by Alan on 6/26/15.
+//  Copyright (c) 2015 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NNActionSheet/NNActionSheetTests/NNActionSheetTests.m
@@ -0,0 +1,40 @@
+//
+//  NNActionSheetTests.m
+//  NNActionSheetTests
+//
+//  Created by Alan on 6/26/15.
+//  Copyright (c) 2015 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NNActionSheetTests : XCTestCase
+
+@end
+
+@implementation NNActionSheetTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
